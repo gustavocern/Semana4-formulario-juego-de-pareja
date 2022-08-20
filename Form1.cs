3926c6ad7259c964c5adf7fb4fc534a47fe86a25
gustavocern <gustavoargueta363@gmail.com>
2022-08-19 21:57:50 -0500

    Ingreso de Primer parte de codigo.

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,34 @@ namespace Semana4_formulario_juego_de_pareja
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        // Each of these letters is an interesting icon
+        // in the Webdings font,
+        // and each icon appears twice in this list
+        List<string> icons = new List<string>()
+    {
+        "!", "!", "N", "N", ",", ",", "k", "k",
+        "b", "b", "v", "v", "w", "w", "z", "z"
+    };
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
         public Form1()
         {
             InitializeComponent();
